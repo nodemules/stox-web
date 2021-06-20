f46d40db91c5c401e6c8fb7c54e82b3c082c434d
brenthaertlein <brenthaertlein@gmail.com>
2021-06-20 11:39:45 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Logo from "./Logo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Check on your favorite stoks!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/Logo.tsx
@@ -0,0 +1,14 @@
+import './Logo.scss'
+
+const Logo = () => {
+
+    return (
+        <div className="logo texture">
+            <div className="title texture">
+                <h1>Stok</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Logo
