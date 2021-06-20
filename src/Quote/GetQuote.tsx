94c7833952e108193f4f13f084b941fbbe08e751
brenthaertlein <brenthaertlein@gmail.com>
2021-06-20 19:30:13 -0400

    #1
    * remove `package-lock.json`
    * add style for `GetQuote` search bar and button

--- src/Quote/GetQuote.tsx
+++ src/Quote/GetQuote.tsx
@@ -1,11 +1,12 @@
-import style from "./GetQuote.module.scss"
+// @ts-ignore
+import {Search, Submit} from "./GetQuote.module.scss"
 import {getQuote} from "../Api/StoxQuoteApi";
 import {ChangeEvent, MouseEvent, useState} from "react";
 import Quote from "./Quote";
 
 const GetQuote = () => {
 
-    const [searchTerm, setSearchTerm] = useState<string>();
+    const [searchTerm, setSearchTerm] = useState<string>("");
     const [quote, setQuote] = useState<any>();
 
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -18,20 +19,25 @@ const GetQuote = () => {
     }
 
     return (
-        <div className={style.GetQuote}>
-            <input
-                type={"text"}
-                placeholder={"Search for a stock using a ticker symbol (e.g. IBM)"}
-                value={searchTerm}
-                onChange={onChange}
-            />
-            <button
-                onClick={search}
-                disabled={!searchTerm}
-            >
-                Stox!
-            </button>
-            <Quote quote={quote} />
+        <div>
+            <form>
+                <input
+                    className={Search}
+                    type="text"
+                    placeholder="Search for a stock (e.g. IBM)"
+                    value={searchTerm}
+                    onChange={onChange}
+                />
+                <button
+                    className={Submit}
+                    type="submit"
+                    onClick={search}
+                    disabled={!searchTerm}
+                >
+                    Stox!
+                </button>
+            </form>
+            <Quote quote={quote}/>
         </div>
     )
 }
