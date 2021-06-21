d8112a6afe311392137f4e6958a9ef250b32cd7e
brenthaertlein <brenthaertlein@gmail.com>
2021-06-20 21:42:22 -0400

    #1
    * remove comments from `SignIn`

--- src/Authentication/SignIn.tsx
+++ src/Authentication/SignIn.tsx
@@ -14,20 +14,13 @@ const SignIn = () => {
         .then((result) => {
             /** @type {firebase.auth.OAuthCredential} */
             const credential = result.credential as firebase.auth.OAuthCredential;
-            // This gives you a Google Access Token. You can use it to access the Google API.
             setAccessToken(credential.accessToken)
-            // The signed-in user info.
             setUser(result.user || undefined);
-            // ...
         }).catch((error) => {
-            // Handle Errors here.
             const errorCode = error.code;
             const errorMessage = error.message;
-            // The email of the user's account used.
             const email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
             const credential = error.credential;
-            // ...
             console.error(`[${errorCode}] ${errorMessage} for ${email} with ${credential}`)
         });
     };
