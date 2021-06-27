a202350806b107d802f4fc5b7fd2bd1fce073ad4
brenthaertlein <brenthaertlein@gmail.com>
2021-06-27 11:45:03 -0400

    #3
    * toggle Trending Tickers section

--- src/Trending/Trending.tsx
+++ src/Trending/Trending.tsx
@@ -1,33 +1,46 @@
 import style from "./Trending.module.scss"
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {getTrending} from "../Api/stox/MarketApi";
 import {GlobalQuote} from "../Quote/Quote";
 import TrendingQuote from "./TrendingQuote";
 import ToggleGroup from "../Components/ToggleGroup";
 
 const Trending = () => {
+    const [open, setOpen] = useState(false)
     const [trending, setTrending] = useState<Array<GlobalQuote>>([])
     const [quoteType, setQuoteType] = useState<String>()
 
     useEffect(() => {
-        getTrending(quoteType).then(setTrending).catch(console.error)
-    }, [quoteType])
+        open && getTrending(quoteType).then(setTrending).catch(console.error)
+    }, [open, quoteType])
+
+    const toggleVisibility = useCallback(() => setOpen(!open), [open])
 
     return (
         <div className={style.Trending}>
-            <div className={style.Title}>
+            <div
+                className={style.Title}
+                onClick={toggleVisibility}
+            >
                 Trending Tickers
             </div>
-            <ToggleGroup
-                elements={["EQUITY", "CRYPTOCURRENCY", "ETF"]}
-                selected={quoteType}
-                onSelected={setQuoteType}
-            />
-            <div className={style.Quotes}>
-                {
-                    trending.map(quote => (<TrendingQuote key={quote.symbol} quote={quote}/>))
-                }
-            </div>
+            {
+                open ?
+                    <>
+                        <ToggleGroup
+                            elements={["EQUITY", "CRYPTOCURRENCY", "ETF"]}
+                            selected={quoteType}
+                            onSelected={setQuoteType}
+                        />
+                        <div className={style.Quotes}>
+                            {
+                                trending.map(quote => (
+                                    <TrendingQuote key={quote.symbol} quote={quote}/>))
+                            }
+                        </div>
+                    </>
+                    : <></>
+            }
         </div>
     )
 }
